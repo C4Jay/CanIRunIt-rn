63131ebe7f49f884ebd427c0509a6aa2655115c5
C4Jay <choolakejay@gmail.com>
2020-04-09 12:27:57 +0530

    gamepost

--- components/gamepost.js
+++ components/gamepost.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class Gamepost extends Component {
+
+    static navigationOptions = {
+        title: 'Test',
+        headerStyle: {
+          backgroundColor: /* '#f4511e' */ Platform.OS === 'android' ? '#050a0e' : '#f4511e',
+        },
+        headerTintColor: 'white',
+        headerTitleStyle: { 
+        },
+    }
 
     state = {
         cpu: '',
@@ -11,11 +21,29 @@ class Gamepost extends Component {
 
     render () {
         return (
-            <View>
-                <Text>{this.props.navigation.getParam('cpu')}</Text>
+            <View style={styles.screen}>
+                <Text style={styles.text}>{this.props.navigation.getParam('cpu')}</Text>
+                <Text style={styles.text}>{this.props.navigation.getParam('gpu')}</Text>
+                <Text style={styles.text}>{this.props.navigation.getParam('ram')}</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: '#fae01e',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 10,
+        fontFamily: 'sans-serif-condensed',
+        fontSize: 28
+    },
+    text: {
+        fontFamily: 'sans-serif-condensed',
+        fontSize: 28
+    }
+})
 
 export default Gamepost;
